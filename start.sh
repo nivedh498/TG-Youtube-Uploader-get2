5040f15c6fc7e681f6892a7c2252e1cac97acc6b
nivedh498 <84920747+nivedh498@users.noreply.github.com>
2022-02-17 13:56:10 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/GreyMatter658/TG-YouTube-Uploader.git /TG-YouTube-Uploader
+cd /TG-YouTube-Uploader
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
